da6c0905e42a121f10349f6664e639466633b1fa
Adinai Isakova <adinaiisakova@MacBook-Pro-Adinai.local>
2024-03-05 15:21:18 -0600

    my terraform second cloud commit

--- main.tf
+++ main.tf
@@ -16,3 +16,11 @@ resource "aws_subnet" "public_1a" {
   availability_zone = "us-east-1"
   cidr_block = "10.0.0.0/26"
 }
+
+
+resource "aws_subnet" "public_1a" {
+  vpc_id = aws_vpc.cloud_vpc.id
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1"
+  cidr_block = "10.0.0.64/26"
+}
